48a360ae41464483b0a40a1a2c6d095be7aaa91c
Muhammad Amr <mimo.mohamed11@gmail.com>
2024-12-10 17:03:05 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+    if (inputBox.value === '') {
+        alert("You must write something!");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        span.classList.add("delete-btn");
+        li.appendChild(span);
+    }
+    inputBox.value = '';
+    saveData();
+}
+
+function deleteTask(element) {
+    element.parentElement.remove();
+    saveData();
+}
+
+listContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    } else if (e.target.classList.contains("delete-btn")) {
+        deleteTask(e.target);
+    }
+}, false);
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data") || "";
+}
+
+showTask();
